34bf0c5113b95f224c282cd627bd50439fbbc019
Tommaso Bellosta <tommaso.bellosta@mail.polimi.it>
2021-10-12 22:51:47 +0200

    mod results script RANS

--- FLAT_PLATE/TURBULENT/RESULTS/LOTW.m
+++ FLAT_PLATE/TURBULENT/RESULTS/LOTW.m
@@ -9,14 +9,14 @@ uTau = sqrt(0.5*U*U*Cf);
 ypVisc = logspace(-2,1,100);
 ypLog = logspace(1,3,100);
 
-u  = T.Momentum_0/T.Density;
+u  = T.Momentum_0./T.Density;
 
 up = u/uTau;
-nu = T.Laminar_Viscosity/T.Density;
+nu = T.Laminar_Viscosity./T.Density;
 yp = uTau*T.Points_1./nu;
 
 
-k = 0.41
+k = 0.41;
 C = 5;
 
 visc = ypVisc;
@@ -28,19 +28,19 @@ ulog = (1/k)*log(ypLog) + C;
 
 figure; grid on;
 
-semilogx(ypVisc,visc,'k', ypLog, ulog, 'k', yp, up, '--k','linew',1.3);
+semilogx(ypVisc,visc,'r', ypLog, ulog, 'b', yp, up, 'k','linew',1.5);
 
 % labels
-% tz=legend('Blasius', 'SU2');
+tz=legend("$u^+ = y^+$", "$u^+ = \frac{1}{k}\log{y^+} + C$", "SU2 SA");
 ty=ylabel('$u^+$');
 tx=xlabel('$y^+$');
 
 % fig stuff
-% legend boxoff
+legend boxoff
 tx.Interpreter='latex';
 ty.Interpreter='latex';
-% tz.Interpreter='latex';
-% tz.Location='best';
+tz.Interpreter='latex';
+tz.Location='best';
 set(gca,'TickLabelInterpreter', 'latex');
 x0=10;
 y0=10;
@@ -49,6 +49,6 @@ height=400;
 set(gcf,'position',[x0,y0,width,height])
 ax = gca;
 ax.FontSize = 18;
-% xlim([1e-1 1e3]);
+xlim([1e-1 1e4]);
 % ylim([0 30]);
 
